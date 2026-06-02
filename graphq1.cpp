cdfc25d377dbbaa2e52f477dcb3a93644f9ce1d0
Vishal <vishalkumarmalladi@gmail.com>
2026-06-02 16:11:58 +0530

    Stored data of operator n1

--- graphq1.cpp
+++ graphq1.cpp
@@ -19,9 +19,22 @@ public:
 //nodes represent operators, edges represent tensors
 class Graph{
 public:
-    unordered_map<string, Operator*> operators;
-    unordered_map<string, Tensor*> tensors;
+    unordered_map<string, Operator> operators;
+    unordered_map<string, Tensor> tensors;
 };
 int main(){
+    Graph graph;
+    //1st operator
+    Operator &n1 = graph.operators["n1"];
+    Tensor &a = graph.tensors["a"];
+    Tensor &x = graph.tensors["x"];
+    Tensor &w1= graph.tensors["w1"];
+    n1.type = "Conv";
+    n1.runtime = 8;
+    n1.inputs = {&x, &w1};
+    n1.output = &a;
+    x.consumers.push_back(&n1);
+    w1.consumers.push_back(&n1);
+    a.producer = &n1;
     return 0;
 }
