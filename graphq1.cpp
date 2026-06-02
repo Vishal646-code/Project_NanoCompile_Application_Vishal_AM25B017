7baf8b13454e92105ec92cc969e7954e9bef9f25
Vishal <vishalkumarmalladi@gmail.com>
2026-06-02 17:18:29 +0530

    Added names and implemented printgraph

--- graphq1.cpp
+++ graphq1.cpp
@@ -3,6 +3,7 @@ using namespace std;
 class Tensor; class Operator;
 class Operator{
 public:
+    string name;
     string type;
     int runtime=0;
     vector<Tensor*> inputs;
@@ -11,6 +12,7 @@ public:
 };
 class Tensor{
 public:
+    string name;
     string dtype;
     vector<int> shape;
     vector<Operator*> consumers;
@@ -37,6 +39,15 @@ int earextime(Operator* node, unordered_map<Operator*, int> &memo){
     memo[node]= maxtime + node->runtime;
     return memo[node];
 }
+
+void printgraph(Graph &graph){
+    for(auto &[tname, ten]: graph.tensors){
+        if(ten.producer == nullptr) continue;
+        for(Operator* consumer : ten.consumers){
+            cout<< ten.producer->name << " -> "<< consumer->name<<'\n';
+        }
+    }
+}
 int main(){
     Graph graph;
     //1st operator
@@ -44,10 +55,14 @@ int main(){
     Tensor &a = graph.tensors["a"];
     Tensor &x = graph.tensors["x"];
     Tensor &w1 = graph.tensors["w1"];
+    n1.name= "n1";
     n1.type = "Conv";
     n1.runtime = 8;
     n1.inputs = {&x, &w1};
     n1.output = &a;
+    a.name= "a";
+    x.name="x";
+    w1.name= "w1";
     x.consumers.push_back(&n1);
     w1.consumers.push_back(&n1);
     a.producer = &n1;
@@ -55,10 +70,12 @@ int main(){
     //2nd operator
     Operator &n2 = graph.operators["n2"];
     Tensor &b = graph.tensors["b"];
+    n2.name= "n2";
     n2.type = "ReLU";
     n2.runtime = 1;
     n2.inputs = {&a};
     n2.output = &b;
+    b.name = "b";
     a.consumers.push_back(&n2);
     b.producer = &n2;
     
@@ -66,10 +83,13 @@ int main(){
     Operator &n3 = graph.operators["n3"];
     Tensor &c = graph.tensors["c"];
     Tensor &w2 = graph.tensors["w2"];
+    n3.name= "n3";
     n3.type = "Conv";
     n3.runtime = 10;
     n3.inputs = {&b, &w2};
     n3.output = &c;
+    c.name= "c";
+    w2.name= "w2";
     b.consumers.push_back(&n3);
     w2.consumers.push_back(&n3);
     c.producer = &n3;
@@ -78,10 +98,13 @@ int main(){
     Operator &n4 = graph.operators["n4"];
     Tensor &s = graph.tensors["s"];
     Tensor &w_skip = graph.tensors["w_skip"];
+    n4.name= "n4";
     n4.type = "Conv";
     n4.runtime = 5;
     n4.inputs = {&x, &w_skip};
     n4.output = &s;
+    s.name= "s";
+    w_skip.name= "w_skip";
     x.consumers.push_back(&n4);
     w_skip.consumers.push_back(&n4);
     s.producer = &n4;
@@ -89,10 +112,12 @@ int main(){
     //5th operator
     Operator &n5 = graph.operators["n5"];
     Tensor &d = graph.tensors["d"];
+    n5.name= "n5";
     n5.type = "Add";
     n5.runtime = 1;
     n5.inputs = {&c, &s};
     n5.output = &d;
+    d.name= "d";
     c.consumers.push_back(&n5);
     s.consumers.push_back(&n5);
     d.producer = &n5;
@@ -100,10 +125,12 @@ int main(){
     //6th operator
     Operator &n6 = graph.operators["n6"];
     Tensor &e = graph.tensors["e"];
+    n6.name= "n6";
     n6.type = "ReLU";
     n6.runtime = 1;
     n6.inputs = {&d};
     n6.output = &e;
+    e.name= "e";
     d.consumers.push_back(&n6);
     e.producer = &n6;
 
@@ -111,10 +138,13 @@ int main(){
     Operator &n7 = graph.operators["n7"];
     Tensor &f = graph.tensors["f"];
     Tensor &scale = graph.tensors["scale"];
+    n7.name= "n7";
     n7.type = "Mul";
     n7.runtime = 1;
     n7.inputs = {&e, &scale};
     n7.output = &f;
+    f.name= "f";
+    scale.name= "scale";
     e.consumers.push_back(&n7);
     scale.consumers.push_back(&n7);
     f.producer = &n7;
@@ -123,14 +153,19 @@ int main(){
     Operator &n8 = graph.operators["n8"];
     Tensor &y = graph.tensors["y"];
     Tensor &bias = graph.tensors["bias"];
+    n8.name= "n8";
     n8.type = "Add";
     n8.runtime = 1;
     n8.inputs = {&f, &bias};
     n8.output = &y;
+    y.name= "y";
+    bias.name= "bias";
     f.consumers.push_back(&n8);
     bias.consumers.push_back(&n8);
     y.producer = &n8;
     
     unordered_map<Operator*, int> memo;
+    cout<<"Earliest execution time of n8 is: "<<earextime(&n8, memo)<<"ms\n";
+    printgraph(graph);
     return 0;
 }
